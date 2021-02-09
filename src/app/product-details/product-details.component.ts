b2c1de930c1d1fc5fbb0b7f0a3b27bb69fa90b1f
mavegit <livegit2@gmail.com>
2021-02-09 12:00:33 +0530

    2 files modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ActivatedRoute} from '@angular/router';
+import {products} from '../products';
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
-
-  constructor() { }
+  product;
+  constructor( private route : ActivatedRoute,) {
+   
+   }
 
   ngOnInit() {
   }
